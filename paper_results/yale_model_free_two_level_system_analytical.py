7ef727bc3defbb20810fd41742be167b8df475a5
Arthur Strauss <arthurostrauss@gmail.com>
2022-08-25 16:49:25 +0800

    Corrected Tensorflow version and updated analytical version
    Issue with gradient tape solved: Declare all variables of interest out of the Gradient Tape context manager

--- paper_results/yale_model_free_two_level_system_analytical.py
+++ paper_results/yale_model_free_two_level_system_analytical.py
@@ -12,32 +12,31 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-"""
-This code sets the simplest RL algorithm (Policy Gradient) for solving a quantum control problem.
-The goal is the following: We have access to a quantum computer (here a simulator provided by IBMQ) containing one 
-qubit. The qubit originally starts in the state |0>, and we would like to apply a quantum gate (operation) to bring it
-to the |1> state. To do so, we have access to a gate parametrized with an angle, and the RL agent must find the optimal
-angle that maximizes the probability of measuring the qubit in the |1> state. Optimal value for amplitude a (angle/2π) is
-0.5. 
-The RL agent chooses its actions (i.e. picks a random value for a) by drawing a number from a Gaussian distribution,
-of mean mu and standard deviation sigma. The trainable parameters are therefore those two latter variables (we expect the mean 
-to be close to 0.5 and the std very low).
-The reward is a binary number obtained upon measurement of the circuit produced (only two possible outcomes can be measured).
+"""This code sets the simplest RL algorithm (Policy Gradient) for solving a quantum control problem. The goal is the 
+following: We have access to a quantum computer (here a simulator provided by IBM Q) containing one qubit. The qubit 
+originally starts in the state |0>, and we would like to apply a quantum gate (operation) to bring it to the |1> 
+state. To do so, we have access to a gate parametrized with an angle, and the RL agent must find the optimal angle 
+that maximizes the probability of measuring the qubit in the |1> state. Optimal value for amplitude a (angle/2π) is 
+0.5. The RL agent chooses its actions (i.e. picks a random value for a) by drawing a number from a Gaussian 
+distribution, of mean mu and standard deviation sigma. The trainable parameters are therefore those two latter 
+variables (we expect the mean to be close to 0.5 and the std very low). The reward is a binary number obtained upon 
+measurement of the circuit produced (only two possible outcomes can be measured). Note that this problem does contain
+only one timestep per episode, meaning that there is no need to introduce any discount factor for future reward (gamma).
 
 """
 
 
-def perform_action(a, shots=1):
+def perform_action(amp, shots=1):
     """
     Execute quantum circuit with parametrized amplitude, retrieve measurement result and assign rewards accordingly
-    :param a: amplitude parameter (given as a np.array of size batchsize)
+    :param amp: amplitude parameter (given as a ndarray of size batchsize)
     :param shots: number of evaluations to be done on the quantum computer (for simplicity stays to 1)
     :return: Reward table (reward for each run in the batch)
     """
     global qc, qasm
 
-    reward_table = np.zeros(np.shape(a))
-    for i, angle in enumerate(a):
+    reward_table = np.zeros(np.shape(amp))
+    for j, angle in enumerate(amp):
         qc.rx(2 * np.pi * angle, 0)  # Add parametrized gate for each amplitude in the batch
         qc.measure(0, 0)  # Measure the qubit
         job = qasm.run(qc, shots=shots)
@@ -46,11 +45,11 @@ def perform_action(a, shots=1):
 
         #  Calculate reward
         if '1' in counts and '0' in counts:
-            reward_table[i] += np.mean(np.array([1] * counts['1'] + [-1] * counts['0']))
+            reward_table[j] += np.mean(np.array([1] * counts['1'] + [-1] * counts['0']))
         elif '0' in counts:
-            reward_table[i] += np.mean([-1] * counts['0'])
+            reward_table[j] += np.mean([-1] * counts['0'])
         else:
-            reward_table[i] += np.mean([1] * counts['1'])
+            reward_table[j] += np.mean([1] * counts['1'])
         qc.clear()
 
     return reward_table  # Shape [batchsize]
@@ -62,16 +61,23 @@ qasm = QasmSimulator()  # Simulation backend (mock quantum computer)
 
 # Hyperparameters for the agent
 n_epochs = 100
-batchsize = 60
-eta = 0.3
+batchsize = 80
+eta = 0.3  # Learning rate for policy gradient
 
-# Trainable parameters
+eta_2 = 1.  # Learning rate for state-value function
+b = np.random.uniform(0, 1)  # Learnable parameter for the value function (baseline/critic)
+# Trainable parameters of the policy (mean and standard deviation of the univariate Gaussian policy)
+# mu = np.random.uniform(0, 1)
+# sigma = 2.
+seed = 5
+np.random.seed(seed)
 mu = np.random.uniform(0, 1)
-sigma = 1.6
-means, std, amps, rewards = np.zeros(n_epochs), np.zeros(n_epochs), np.zeros(n_epochs), np.zeros(n_epochs)
+sigma = np.random.uniform(1, 2)
+
+means, std, amps, rewards = np.zeros(n_epochs+1), np.zeros(n_epochs+1), np.zeros(n_epochs), np.zeros(n_epochs)
 
 for i in range(n_epochs):
-    a = np.random.normal(loc=mu, scale=sigma, size=batchsize)
+    a = np.random.normal(loc=mu, scale=np.abs(sigma), size=batchsize)
     reward = perform_action(a)
 
     amps[i] = np.mean(a)
@@ -79,25 +85,29 @@ for i in range(n_epochs):
     means[i] = mu
     std[i] = sigma
 
-    # REINFORCE Algorithm, update the parameters with the derived analytical formulas
+    # REINFORCE Algorithm, update the parameters with the derived analytical formulas for gradients
+    b += eta_2 * np.mean(reward)
     mu += eta * np.mean(reward * (a - mu) / sigma ** 2)
     sigma += eta * np.mean(reward * ((a - mu) ** 2 / sigma ** 3 - 1 / sigma))
-    if sigma < 0:
-        sigma = 1
+    # if sigma < 0:
+    #     sigma = 1.
 
 final_mean = mu
 final_std = sigma
+means[-1] = final_mean
+std[-1] = final_std
 
 print("means: ", means, '\n')
 print("stds: ", std, '\n')
 print("amplitudes: ", amps, '\n')
 print("average rewards: ", rewards)
 
-number_of_steps = 50
-x = np.linspace(-2., 2., 300)
+number_of_steps = 20
+x = np.linspace(-2., 2., 600)
 fig, (ax1, ax2) = plt.subplots(1, 2)
-for i in range(0, n_epochs, number_of_steps):
-    ax1.plot(x, norm.pdf(x, loc=means[i], scale=std[i]), label=f'{i}')
+for i in range(0, n_epochs+1, number_of_steps):
+    ax1.plot(x, norm.pdf(x, loc=means[i], scale=np.abs(std[i])), label=f'{i}')
+print(i)
 
 ax1.set_xlabel("Action, a")
 ax1.set_ylabel("Probability density")
--- paper_results/yale_model_free_two_level_system_example.py
+++ paper_results/yale_model_free_two_level_system_example.py
@@ -16,30 +16,28 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-"""
-This code sets the simplest RL algorithm (Policy Gradient) for solving a quantum control problem.
-The goal is the following: We have access to a quantum computer (here a simulator provided by IBMQ) containing one 
-qubit. The qubit originally starts in the state |0>, and we would like to apply a quantum gate (operation) to bring it
-to the |1> state. To do so, we have access to a gate parametrized with an angle, and the RL agent must find the optimal
-angle that maximizes the probability of measuring the qubit in the |1> state. Optimal value for amplitude a (angle/2π) is
-0.5. 
-The RL agent chooses its actions (that is picks a random value for a) by drawing a number from a Gaussian distribution,
-of mean mu and variance sigma_2. The trainable parameters are therefore those two latter variables (we expect the mean 
-to be close to 0.5 and the variance very low).
-The reward is a binary number obtained upon measurement of the circuit produced (only two possible outcomes can be measured).
+"""This code sets the simplest RL algorithm (Policy Gradient) for solving amp quantum control problem. The goal is the 
+following: We have access to amp quantum computer (here amp simulator provided by IBM Q) containing one qubit. The qubit 
+originally starts in the state |0>, and we would like to apply amp quantum gate (operation) to bring it to the |1> 
+state. To do so, we have access to amp gate parametrized with an angle, and the RL agent must find the optimal angle 
+that maximizes the probability of measuring the qubit in the |1> state. Optimal value for amplitude amp (angle/2π) is 
+0.5. The RL agent chooses its actions (that is picks amp random value for amp) by drawing amp number from amp Gaussian 
+distribution, of mean mu and standard deviation sigma. The trainable parameters are therefore those two latter 
+variables (we expect the mean to be close to 0.5 and the variance very low). The reward is amp binary number obtained 
+upon measurement of the circuit produced (only two possible outcomes can be measured). 
 
 """
 
 
-def perform_action(a: tf.Tensor, shots=1):
+def perform_action(amp: tf.Tensor, shots=1):
     """
     Execute quantum circuit with parametrized amplitude, retrieve measurement result and assign rewards accordingly
-    :param a: amplitude parameter
+    :param amp: amplitude parameter
     :param shots: number of evaluations to be done on the quantum computer (for simplicity stays to 1)
     :return: Reward table (reward for each run in the batch)
     """
     global qc, qasm
-    angles = np.array(a)
+    angles = np.array(amp)
     assert len(np.shape(angles)) == 1, f'What happens : {np.shape(angles)}'
 
     reward_table = np.zeros(np.shape(angles))
@@ -48,7 +46,7 @@ def perform_action(a: tf.Tensor, shots=1):
         qc.measure(0, 0)  # Measure the qubit
         job = qasm.run(qc, shots=shots)
         result = job.result()
-        counts = result.get_counts(qc)  # Returns a dictionary with keys '0' and '1' with number of counts for each key
+        counts = result.get_counts(qc)  # Returns dictionary with keys '0' and '1' with number of counts for each key
 
         #  Calculate reward
         if '1' in counts and '0' in counts:
@@ -69,34 +67,34 @@ qasm = QasmSimulator()  # Simulation backend (mock quantum computer)
 # Hyperparameters for the agent
 n_epochs = 100
 batchsize = 50
-eta = 0.01
+eta = 0.1
+seed = 5
+tf.random.set_seed(seed)
 # optimizer = Adam(learning_rate=eta)
 optimizer = SGD(learning_rate=eta)
-mu = tf.Variable(initial_value=0.45, trainable=True, name="mean")
-sigma = tf.Variable(initial_value=0.1, trainable=True, name="variance")
-losses, means, std, amps, rewards = np.zeros(n_epochs), np.zeros(n_epochs), np.zeros(n_epochs), np.zeros(
+mu = tf.Variable(initial_value=np.random.uniform(0, 1), trainable=True, name="mean")
+sigma = tf.Variable(initial_value=np.random.uniform(1, 2), trainable=True, name="variance")
+losses, means, std, amps, rewards = np.zeros(n_epochs), np.zeros(n_epochs + 1), np.zeros(n_epochs + 1), np.zeros(
     n_epochs), np.zeros(n_epochs)
 grad_list = [None] * n_epochs
 
 
-def normal_distrib(a, mu, sigma):
-    # return math.divide(math.exp(math.divide(math.pow(a - mu, 2), -2 * math.pow(sigma, 2))),
+
+def normal_distrib(amp, mu, sigma):
+    # return math.divide(math.exp(math.divide(math.pow(amp - mu, 2), -2 * math.pow(sigma, 2))),
     #                    math.sqrt(2 * np.pi * math.pow(sigma, 2)))
-    return math.exp(-(a - mu) ** 2 / (2 * sigma ** 2)) / math.sqrt(2 * np.pi * sigma ** 2)
+    return math.exp(-(amp - mu) ** 2 / (2 * sigma ** 2)) / math.sqrt(2 * np.pi * sigma ** 2)
 
 
 for i in range(n_epochs):
+    a = tf.random.normal([batchsize], mean=mu, stddev=sigma)
+    reward = perform_action(a)
     with tf.GradientTape() as tape:
-        tape.watch(mu)
-        tape.watch(sigma)
-        a = tf.random.normal([batchsize], mean=mu, stddev=sigma)
-        reward = perform_action(a)
-
         # Calculate expected return (to be maximized, therefore the minus sign placed in the function below will be
-        # useful when applying gradients which minimize the loss),
-        # E[R*log(proba(a)] where proba is a gaussian probability density (cf paper of reference, educational example)
+        # useful when applying gradients which minimize the loss), E[R*log(proba(amp)] where proba is amp gaussian
+        # probability density (cf paper of reference, educational example)
 
-        # loss = tf.reduce_mean(-reward * math.log(normal_distrib(a, mu, sigma)))
+        # loss = tf.reduce_mean(-reward * math.log(normal_distrib(amp, mu, sigma)))
         loss = tf.reduce_mean(
             -reward * math.log(math.exp(-(a - mu) ** 2 / (2 * sigma ** 2)) / math.sqrt(2 * np.pi * sigma ** 2)))
 
@@ -115,19 +113,24 @@ for i in range(n_epochs):
 final_mean = np.array(mu)
 final_std = np.array(tf.sqrt(sigma))
 
+means[-1] = final_mean
+std[-1] = final_std
+
 print("means: ", means, '\n')
 print("stds: ", std, '\n')
-print("amps: ", amps, '\n')
-print("rewards: ", rewards)
-x = np.linspace(-1., 1., 300)
+print("amplitudes: ", amps, '\n')
+print("average rewards: ", rewards)
+
+number_of_steps = 20
+x = np.linspace(-2., 2., 300)
 fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.plot(x, norm.pdf(x, loc=final_mean, scale=final_std))
-ax2.plot(range(n_epochs), rewards)
+for i in range(0, n_epochs + 1, number_of_steps):
+    ax1.plot(x, norm.pdf(x, loc=means[i], scale=np.abs(std[i])), label=f'{i}')
+
 ax1.set_xlabel("Action, a")
 ax1.set_ylabel("Probability density")
 ax2.plot(range(n_epochs), rewards)
 ax2.set_xlabel("Epoch")
 ax2.set_ylabel("Expected reward")
+ax1.legend()
 plt.show()
-
-
