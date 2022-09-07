7987a78d8b2ad3752713437b318b23d8956e58ea
Arthur Strauss <arthurostrauss@gmail.com>
2022-09-07 18:28:26 +0800

    Implemented working version
    Solution was obtained by combining in one single loss function the actor and the critic contributions, and using one single optimizer (Adam, SGD does not work for the same parameters, and does not seem to work at all based on the few trials intended)
    
    Added fidelity computation and prepared the ground for targeting other states (tried on state of the Hadamard basis the naive way but not working at the moment)
    Overall, success of the algorithm is extremely (too much compared to original paper) to hyperparams such as learning rate (not the same as original paper for the working case), n_epochs, and seed. Did not try to change the batchsize in the few trials

--- paper_results/yale_model_free_two_level_system_example.py
+++ paper_results/yale_model_free_two_level_system_example.py
@@ -6,29 +6,27 @@ Code example reproducing Educational Example described in Appendix A of the pape
  Created on 05/08/2022
 """
 
+import tensorflow as tf
+import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.providers.aer import QasmSimulator
-from qiskit.assembler import RunConfig
+import qiskit.quantum_info as qi
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 from tqdm import tqdm
 from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
-import tensorflow as tf
-from tensorflow.python.keras.losses import MSE
-import tensorflow.python.ops.math_ops as math
-import numpy as np
+# from tensorflow.python.keras.losses import MSE
 from scipy.stats import norm
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from typing import Union, Optional
+from typing import Union
 
-"""This code sets the simplest RL algorithm (Policy Gradient) for solving amp quantum control problem. The goal is the 
-following: We have access to amp quantum computer (here amp simulator provided by IBM Q) containing one qubit. The qubit 
-originally starts in the state |0>, and we would like to apply amp quantum gate (operation) to bring it to the |1> 
-state. To do so, we have access to amp gate parametrized with an angle, and the RL agent must find the optimal angle 
+"""This code sets the simplest RL algorithm (Policy Gradient) for solving a quantum control problem. The goal is the 
+following: We have access to a quantum computer (here a simulator provided by IBM Q) containing one qubit. The qubit 
+originally starts in the state |0>, and we would like to apply a quantum gate (operation) to bring it to the |1> 
+state. To do so, we have access to a gate parametrized with an angle, and the RL agent must find the optimal angle 
 that maximizes the probability of measuring the qubit in the |1> state. Optimal value for amplitude amp (angle/2π) is 
-0.5. The RL agent chooses its actions (that is picks amp random value for amp) by drawing amp number from amp Gaussian 
+0.5. The RL agent chooses its actions (that is picks a random value for amp) by drawing a number from a Gaussian 
 distribution, of mean mu and standard deviation sigma. The trainable parameters are therefore those two latter 
-variables (we expect the mean to be close to 0.5 and the variance very low). The reward is amp binary number obtained 
+variables (we expect the mean to be close to 0.5 and the variance very low). The reward is a binary number obtained 
 upon measurement of the circuit produced (only two possible outcomes can be measured). 
 
 """
@@ -41,13 +39,17 @@ def perform_action(amp: Union[tf.Tensor, np.array], shots=1):
     :param shots: number of evaluations to be done on the quantum computer (for simplicity stays to 1)
     :return: Reward table (reward for each run in the batch)
     """
-    global qc, qasm
+    global qc, qasm, seed
     angles = np.array(amp)
+    density_matrix = np.zeros([2, 2], dtype='complex128')
     assert len(np.shape(angles)) == 1, f'What happens : {np.shape(angles)}'
 
     reward_table = np.zeros(np.shape(angles))
-    for i, angle in enumerate(angles):
+    for j, angle in enumerate(angles):
         qc.rx(2 * np.pi * angle, 0)  # Add parametrized gate for each amplitude in the batch
+        # qc.h(0)
+        q_state = qi.Statevector.from_instruction(qc)
+        density_matrix += np.array(q_state.to_operator()) / len(angles)
         qc.measure(0, 0)  # Measure the qubit
         job = qasm.run(qc, shots=shots, seed_simulator=seed)
         result = job.result()
@@ -55,62 +57,69 @@ def perform_action(amp: Union[tf.Tensor, np.array], shots=1):
 
         #  Calculate reward
         if '1' in counts and '0' in counts:
-            reward_table[i] += np.mean(np.array([1] * counts['1'] + [-1] * counts['0']))
+            reward_table[j] += np.mean(np.array([1] * counts['1'] + [-1] * counts['0']))
         elif '0' in counts:
-            reward_table[i] += np.mean([-1] * counts['0'])
+            reward_table[j] += np.mean([-1] * counts['0'])
         else:
-            reward_table[i] += np.mean([1] * counts['1'])
+            reward_table[j] += np.mean([1] * counts['1'])
         qc.clear()
-
-    return reward_table  # Shape [batchsize]
+    return reward_table, qi.DensityMatrix(density_matrix)  # Shape [batchsize]
 
 
 # Variables to define environment
-seed = 2024
+seed = 2906
 tf.random.set_seed(seed)
 np.random.seed(seed)
-config = RunConfig(shots=1, seed_simulator=seed)
 qc = QuantumCircuit(1, 1, name="qc")  # Two-level system of interest, 1 qubit
 qasm = QasmSimulator(method="statevector")  # Simulation backend (mock quantum computer)
 
+target_state = qi.DensityMatrix(np.array([[0.], [1.]]) @ np.array([[0., 1.]]))
+# target_state = qi.DensityMatrix(0.5 * np.array([[1.], [-1.]]) @ np.array([[1., -1.]]))
 # Hyperparameters for the agent
 insert_baseline = True  # Indicate if you want the actor-critic version (True) or simple REINFORCE (False)
 use_PPO = True
-epsilon = 0.2  # Parameter for clipping value (PPO)
-n_epochs = 150
-batchsize = 100
-eta = 0.1  # Learning rate for policy update step
+epsilon = 0.08  # Parameter for clipping value (PPO)
+n_epochs = 50
+batchsize = 30
+eta = 0.2  # Learning rate for policy update step
 eta_2 = 0.1  # Learning rate for critic (value function) update step
 
+concurrent_optimization = True  # Fix if optimization of actor and critic should be done by same optimizer or separately
 
-optimizer_actor = Adam(learning_rate=eta)
-optimizer_critic = Adam(learning_rate=eta_2)
+if concurrent_optimization:
+    # Choose optimizer of your choice by commenting irrelevant line
+    optimizer = Adam(learning_rate=eta)
+    # optimizer = SGD(learning_rate=eta)
+else:
+    # Choose optimizer of your choice by commenting irrelevant line
+    optimizer_actor, optimizer_critic = Adam(learning_rate=eta), Adam(learning_rate=eta_2)
+    # optimizer_actor, optimizer_critic = SGD(learning_rate=eta), SGD(learning_rate=eta_2)
 
 # Policy parameters
-
-mu = tf.Variable(initial_value=np.random.uniform(0., 1.), trainable=True, name="µ")
-sigma = tf.Variable(initial_value=np.random.uniform(5., 7.), trainable=True, name="sigma")
+mu = tf.Variable(initial_value=tf.random.normal([], stddev=0.05), trainable=True, name="µ")
+sigma = tf.Variable(initial_value=1., trainable=True, name="sigma")
+sigma_eps = 1e-6  # for numerical stability
 
 # Old parameters are updated with one-step delay, necessary for PPO implementation
+
 mu_old = tf.Variable(initial_value=mu, trainable=False)
 sigma_old = tf.Variable(initial_value=sigma, trainable=False)
 
 # Critic parameter (single state-independent baseline b)
 if insert_baseline:
-    b = tf.Variable(initial_value=np.random.uniform(0., 1.), trainable=True, name="baseline")
-else:  # If implementation done without critic, leave the variable to 0 during all training
     b = tf.Variable(initial_value=0., trainable=True, name="baseline")
-
-
+else:
+    b = tf.Variable(initial_value=0., trainable=False, name="baseline")
 #  Keep track of variables (when script will be functional, do some saving to external file)
-losses, means, stds, amps, rewards = np.zeros(n_epochs), np.zeros(n_epochs + 1), np.zeros(n_epochs + 1), np.zeros(
-    n_epochs), np.zeros([n_epochs, batchsize])
+means, stds, amps, rewards = np.zeros(n_epochs + 1), np.zeros(n_epochs + 1), np.zeros(
+    [n_epochs, batchsize]), np.zeros([n_epochs, batchsize])
 baselines = np.zeros(n_epochs + 1)
+fidelities = np.zeros(n_epochs)
 grad_list = [None] * n_epochs
+grad3_list = [None] * n_epochs
 
 
 def normal_distrib(amp: Union[tf.Tensor, np.array], mean: Union[tf.Variable, float], std: Union[tf.Variable, float]):
-
     """
     Compute probability density of Gaussian distribution evaluated at amplitude amp
     :param amp: amplitude/angle chosen by the random selection
@@ -120,91 +129,115 @@ def normal_distrib(amp: Union[tf.Tensor, np.array], mean: Union[tf.Variable, flo
     """
     # return math.divide(math.exp(math.divide(math.pow(amp - mu, 2), -2 * math.pow(sigma, 2))),
     #                    math.sqrt(2 * np.pi * math.pow(sigma, 2)))
-    return math.exp(-(amp - mean) ** 2 / (2 * std ** 2)) / math.sqrt(2 * np.pi * std ** 2)
+    return tf.exp(-(amp - mean) ** 2 / (2 * std ** 2 + sigma_eps)) / tf.sqrt(2 * np.pi * (std ** 2 + sigma_eps))
 
 
 for i in tqdm(range(n_epochs)):
-    # a = tf.random.normal([batchsize], mean=mu, stddev=sigma, seed=seed)
-    a = np.random.normal(mu, sigma, batchsize)
-    reward = perform_action(a)
+    # Sample action from policy (Gaussian distribution with parameters mu and sigma)
+    a = tf.random.normal([batchsize], mean=mu, stddev=tf.abs(sigma), seed=seed)
+    # a = np.random.normal(mu, np.abs(sigma), batchsize)
 
+    # Run quantum circuit to retrieve rewards (in this example, only one time step)
+    reward, dm_observed = perform_action(a)
+    fidelities[i] = qi.state_fidelity(target_state, dm_observed)
     with tf.GradientTape(persistent=True) as tape:
 
-        """Calculate return (to be maximized, therefore the minus sign placed in the function below will be
-        useful when applying gradients which minimize the loss), E[R*log(proba(amp)] where proba is the gaussian
+        """
+        Calculate return (to be maximized, therefore the minus sign placed in front of the loss 
+        since applying gradients minimize the loss), E[R*log(proba(amp)] where proba is the gaussian
         probability density (cf paper of reference, educational example).
         In case of the PPO, loss function is slightly changed.
         """
-        advantage = reward - b  # If not using the critic, then b=0, and we just have the reward
-        if use_PPO:
-            ratio = normal_distrib(a, mu, sigma) / normal_distrib(a, mu_old, sigma_old)
-            loss = - tf.reduce_mean(math.minimum(advantage * ratio,
-                                                 advantage * tf.clip_by_value(ratio, 1 - epsilon, 1 + epsilon)))
-        else:
-            loss = -tf.reduce_mean(
-                advantage * math.log(normal_distrib(a, mu, sigma)))
-            # loss = tf.reduce_mean(-reward * math.log(normal_distrib(amp, mu, sigma)))
 
-        loss2 = MSE(reward, b)  # Loss for the critic (Mean square error between return and the baseline)
+        advantage = reward - b  # If not using the critic (baseline), then b=0, and we are left with the reward
+        if use_PPO:
+            ratio = normal_distrib(a, mu, sigma) / (
+                    sigma_eps + normal_distrib(a, mu_old, sigma_old))
+            # Avoid division by 0 with small sigma_eps
+
+            actor_loss = - tf.reduce_mean(tf.minimum(advantage * ratio,
+                                                     advantage * tf.clip_by_value(ratio, 1 - epsilon, 1 + epsilon)))
+        else:  # REINFORCE algorithm
+            actor_loss = - tf.reduce_mean(advantage * tf.math.log(normal_distrib(a, mu, sigma)))
+
+        if insert_baseline:
+            # loss2 = MSE(reward, b)  # Loss for the critic (Mean square error between return and the baseline)
+            critic_loss = tf.reduce_mean(advantage ** 2)
+        combined_loss = actor_loss + critic_loss
 
     # Compute gradients
-    grads = tape.gradient(loss, [mu, sigma])
-    grads2 = tape.gradient(loss2, b)
+    grad_clip = 0.001
+    policy_grads = tape.gradient(actor_loss, [mu, sigma])
+    if insert_baseline:
+        value_grads = tape.gradient(critic_loss, b)
+        combined_grads = tape.gradient(combined_loss, [mu, sigma, b])
+        # combined_grads = tf.clip_by_value(grad3, -grad_clip, grad_clip)
 
-    amps[i] = np.array(tf.reduce_mean(a))
-    losses[i] = np.array(loss)
+    # For PPO, update old parameters to have access to "old" policy
+    if use_PPO:
+        mu_old.assign(mu)
+        sigma_old.assign(sigma)
+
+    amps[i] = np.array(a)
     rewards[i] = reward
     means[i] = np.array(mu)
     stds[i] = np.array(sigma)
     baselines[i] = np.array(b)
-    grad_list[i] = grads + grads2
-
-    # For PPO, update old parameters to have easy access to "old" policy
-    mu_old = mu
-    sigma_old = sigma
 
     # Apply gradients
-    optimizer_actor.apply_gradients(zip(grads, (mu, sigma)))
-    if insert_baseline:
-        optimizer_critic.apply_gradients(zip([grads2], [b]))
+    if concurrent_optimization:
+        optimizer.apply_gradients(zip(combined_grads, tape.watched_variables()))
+
+    else:
+        optimizer_actor.apply_gradients(zip(policy_grads, (mu, sigma)))
+        if insert_baseline:
+            optimizer_critic.apply_gradients(zip([value_grads], [b]))
 
 means[-1] = np.array(mu)
 stds[-1] = np.array(sigma)
 baselines[-1] = np.array(b)
-print("means: ", means, '\n')
-print("stds: ", stds, '\n')
-print("amplitudes: ", amps, '\n')
-print("average rewards: ", np.mean(rewards, axis=1))
+print("means: ", means, '\n', "stds: ", stds, '\n')
+print("drawn amplitudes: ", amps, '\n')
+print("average rewards: ", np.mean(rewards, axis=1), '\n')
+print("fidelities:", fidelities)
 
 
 #  Plotting results
-def plot_examples(colormaps, ax, reward_table):
+def plot_examples(ax, reward_table):
     """
     Helper function to plot data with associated colormap, used for plotting the reward per each epoch and each episode
+    (From original repo associated to the paper https://github.com/v-sivak/quantum-control-rl)
     """
 
-    ax.pcolormesh(reward_table.transpose(), cmap=colormaps, rasterized=True, vmin=-1, vmax=1)
-    ax.set_xlabel("Epoch")
-    ax.set_ylabel("Episode")
+    vals = np.where(reward_table == 1, 0.6, -0.9)
+
+    ax.pcolormesh(np.transpose(vals), cmap='RdYlGn', vmin=-1, vmax=1)
+
+    ax.set_xticks(np.arange(0, vals.shape[0], 1), minor=True)
+    ax.set_yticks(np.arange(0, vals.shape[1], 1), minor=True)
+    ax.grid(which='both', color='w', linestyle='-')
+    ax.set_aspect('equal')
+    ax.set_ylabel('Episode')
+    ax.set_xlabel('Epoch')
     plt.show()
 
 
 number_of_steps = 10
-x = np.linspace(-2., 2., 200)
+x = np.linspace(-1., 1., 100)
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 # Plot probability density associated to updated parameters for a few steps
 for i in range(0, n_epochs + 1, number_of_steps):
-    ax1.plot(x, norm.pdf(x, loc=means[i], scale=np.abs(stds[i])), '.-', label=f'{i}')
+    ax1.plot(x, norm.pdf(x, loc=means[i], scale=np.abs(stds[i])), '-o', label=f'{i}')
 
 ax1.set_xlabel("Action, a")
 ax1.set_ylabel("Probability density")
+ax1.set_ylim(0., 20)
 #  Plot return as a function of epochs
 ax2.plot(np.mean(rewards, axis=1), '-.', label='Reward')
 ax2.set_xlabel("Epoch")
 ax2.set_ylabel("Expected reward")
-ax2.plot(baselines, '.-', label='baseline')
+ax2.plot(baselines, '-.', label='baseline')
+ax2.plot(fidelities, '-o', label='Fidelity')
 ax2.legend()
 ax1.legend()
-
-cmap = ListedColormap(["blue", "Orange"])
-plot_examples(cmap, ax3, rewards)
+plot_examples(ax3, rewards)
