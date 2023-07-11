Corrected circuit fidelity computation
Added new benchmark of average gate fidelity for each gate instance in the circuit, on top of the now functioning circuit fidelity (over the original circuit, not the one generated for training with the input state preparation)

quantumenvironment.py
@@ -31,11 +31,12 @@ from qiskit_experiments.library import ProcessTomography, CrossResonanceHamilton
 from qiskit_experiments.library.tomography.basis import PauliPreparationBasis  # , Pauli6PreparationBasis
 from qiskit_experiments.framework import BatchExperiment
 # Qiskit Primitive: for computing Pauli expectation value sampling easily
-from qiskit.primitives import Estimator, BackendEstimator
-from qiskit_ibm_runtime import Estimator as Runtime_Estimator, IBMBackend as Runtime_Backend, Options, Session, QiskitRuntimeService
+from qiskit.primitives import BaseEstimator, Estimator, BackendEstimator
+from qiskit_ibm_runtime import Estimator as Runtime_Estimator, IBMBackend as Runtime_Backend, Options, Session, \
+    QiskitRuntimeService
 from qiskit_aer.primitives import Estimator as Aer_Estimator
 from qiskit_aer.backends.aerbackend import AerBackend
-from qiskit_aer.backends import AerSimulator, UnitarySimulator, StatevectorSimulator
+from qiskit_aer.backends import AerSimulator  # , UnitarySimulator, StatevectorSimulator
 
 import numpy as np
 from itertools import product, permutations
@@ -74,7 +75,8 @@ def perform_standard_calibrations(backend: DynamicsBackend, calibration_files: O
     single_qubit_errors = {(qubit,): 0.0 for qubit in qubits}
 
     control_channel_map = backend.options.control_channel_map or {(qubits[0], qubits[1]): index
-                                                                  for index, qubits in enumerate(permutations(qubits, 2))}
+                                                                  for index, qubits in
+                                                                  enumerate(permutations(qubits, 2))}
     if backend.options.control_channel_map:
         physical_control_channel_map = {(qubit_pair[0], qubit_pair[1]): backend.control_channel((qubit_pair[0],
                                                                                                  qubit_pair[1]))
@@ -87,7 +89,7 @@ def perform_standard_calibrations(backend: DynamicsBackend, calibration_files: O
     two_qubit_properties = {qubits: None for qubits in control_channel_map}
     two_qubit_errors = {qubits: 0.0 for qubits in control_channel_map}
     standard_gates: Dict[str, Gate] = get_standard_gate_name_mapping()  # standard gate library
-    fixed_phase_gates, fixed_phases = ["z", "s", "sdg", "t", "tdg"], np.pi * np.array([1, 1/2, -1/2, 1/4, -1/4])
+    fixed_phase_gates, fixed_phases = ["z", "s", "sdg", "t", "tdg"], np.pi * np.array([1, 1 / 2, -1 / 2, 1 / 4, -1 / 4])
     other_gates = ["rz", "id", "h", "x", "sx", "reset"]
     single_qubit_gates = fixed_phase_gates + other_gates
     two_qubit_gates = ["cx", "ecr"]
@@ -100,13 +102,13 @@ def perform_standard_calibrations(backend: DynamicsBackend, calibration_files: O
     else:
         cals = Calibrations(coupling_map=coupling_map, control_channel_map=physical_control_channel_map,
                             libraries=[FixedFrequencyTransmon(basis_gates=["x", "sx"]),
-                                       EchoedCrossResonance(basis_gates=['cr45p','cr45m', 'ecr', 'rzx'])])
+                                       EchoedCrossResonance(basis_gates=['cr45p', 'cr45m', 'ecr', 'rzx'])])
     if len(target.instruction_schedule_map().instructions) <= 1:  # Check if instructions have already been added
         for gate in single_qubit_gates:
             target.add_instruction(standard_gates[gate], properties=single_qubit_properties)
         if num_qubits > 1:
             target.add_instruction(CrossResonanceHamiltonian.CRPulseGate(width=Parameter("width")),
-                                   properties=two_qubit_properties )
+                                   properties=two_qubit_properties)
             for gate in two_qubit_gates:
                 target.add_instruction(standard_gates[gate], properties=two_qubit_properties)
             target.build_coupling_map(two_q_gate=two_qubit_gates[0])
@@ -150,7 +152,7 @@ def perform_standard_calibrations(backend: DynamicsBackend, calibration_files: O
         s_schedule = block_to_schedule(target.get_calibration('s', (qubit,)))
         h_schedule = pulse.Schedule(s_schedule, sx_schedule, s_schedule, name="h")
         target.update_instruction_properties('h', (qubit,), properties=InstructionProperties(calibration=h_schedule,
-                                                                                    error=0.0))
+                                                                                             error=0.0))
 
     print("All single qubit calibrations are done")
     # cals.save(file_type="csv", overwrite=True, file_prefix="Custom" + backend.name)
@@ -159,9 +161,9 @@ def perform_standard_calibrations(backend: DynamicsBackend, calibration_files: O
     print(control_channel_map)
     for qubit_pair in control_channel_map:
         print(qubit_pair)
-        cr_ham_exp = CrossResonanceHamiltonian(physical_qubits = qubit_pair, flat_top_widths=np.linspace(0, 5000, 17),
+        cr_ham_exp = CrossResonanceHamiltonian(physical_qubits=qubit_pair, flat_top_widths=np.linspace(0, 5000, 17),
                                                backend=backend)
-        print("Calibrating CR for qubits", qubit_pair,"...")
+        print("Calibrating CR for qubits", qubit_pair, "...")
         data_cr = cr_ham_exp.run().block_for_results()
         exp_results[qubit_pair] = data_cr
 
@@ -189,7 +191,6 @@ def _calculate_chi_target_state(target_state: Dict, n_qubits: int):
 
 
 def _define_target(target: Dict):
-
     if "register" in target:
         assert isinstance(target["register"], (List, QuantumRegister)), "Register should be of type List[int] " \
                                                                         "or Quantum Register"
@@ -237,7 +238,7 @@ def _define_target(target: Dict):
             layout = Layout.generate_trivial_layout(q_register)
 
         assert gate.num_qubits == len(q_register), f"Target gate number of qubits ({gate.num_qubits}) " \
-                                                     f"incompatible with indicated 'register' ({len(tgt_register)})"
+                                                   f"incompatible with indicated 'register' ({len(tgt_register)})"
         if 'input_states' not in target:
             target['input_states'] = [{"circuit": PauliPreparationBasis().circuit(s).decompose()}
                                       for s in product(range(4), repeat=len(tgt_register))]
@@ -299,7 +300,7 @@ class QuantumEnvironment:
         elif Qiskit_config is not None:
             self._config_type = "Qiskit"
             self._config: Qiskit_config = Qiskit_config
-            self.target, self.target_type, self.tgt_register, self._n_qubits, self.layout = _define_target(target)
+            self.target, self.target_type, self.tgt_register, self._n_qubits, self._layout = _define_target(target)
 
             self._d = 2 ** self.n_qubits
             self.c_factor = c_factor
@@ -312,26 +313,27 @@ class QuantumEnvironment:
             estimator_options: Optional[Union[Options, Dict]] = Qiskit_config.estimator_options
 
             if self.abstraction_level == "circuit":  # Either state-vector simulation (native) or AerBackend provided
-                if self.target_type == "state":
-                    self._benchmark_backend = StatevectorSimulator()
-                else:
-                    self._benchmark_backend = UnitarySimulator()
+                # if self.target_type == "state":
+                #     self._benchmark_backend = StatevectorSimulator()
+                # else:
+                #     self._benchmark_backend = UnitarySimulator()
                 if isinstance(self.backend, AerBackend):
                     # Estimator taking noise model into consideration, have to provide an AerBackend
                     # TODO: Extract from TranspilationOptions a dict that can go in following definition
-                    self.estimator = Aer_Estimator(backend_options=self.backend.options,
-                                                   transpile_options={'initial_layout': self.layout})
+                    self._estimator: BaseEstimator = Aer_Estimator(backend_options=self.backend.options,
+                                                   transpile_options={'initial_layout': self._layout})
                 else:  # No backend specified
-                    self.estimator = Estimator()  # Estimator based on state-vector simulation
+                    self._estimator: BaseEstimator = Estimator(
+                        options={"initial_layout": self._layout})  # Estimator based on state-vector simulation
             elif self.abstraction_level == 'pulse':
-                self.estimator = BackendEstimator(self.backend)
+                self._estimator: BaseEstimator = BackendEstimator(self.backend, options={"initial_layout": self._layout})
 
                 if isinstance(self.backend, DynamicsBackend):
                     if self.config.do_calibrations:
                         calibration_files: List[str] = Qiskit_config.calibration_files
                         self.calibrations, self.exp_results = perform_standard_calibrations(self.backend,
                                                                                             calibration_files)
-                    self._benchmark_backend= self.backend
+                    self._benchmark_backend = self.backend
                 # For benchmarking the gate at each epoch, set the tools for Pulse level simulator
                 self.solver: Solver = Qiskit_config.solver
                 if Qiskit_config.solver is None:
@@ -347,9 +349,9 @@ class QuantumEnvironment:
                     self.ground_state = Array(np.array([1.0] + [0.0] * (self.model_dim ** 2 - 1)))
 
             if isinstance(self.backend, Runtime_Backend):  # Real backend, or Simulation backend from Runtime Service
-                self.estimator = Runtime_Estimator(session=Session(self.backend.service, self.backend),
-                                                   options=estimator_options)
-                self.estimator.set_options(initial_layout=self.layout)
+                self._estimator: BaseEstimator = Runtime_Estimator(session=Session(self.backend.service, self.backend),
+                                                    options=estimator_options)
+                self._estimator.set_options(initial_layout=self._layout)
 
         elif QUA_config is not None:
             self._config_type = "QUA"
@@ -403,7 +405,6 @@ class QuantumEnvironment:
         # Retrieve Pauli observables to sample, and build a weighted sum to feed the Estimator primitive
         observables = SparsePauliOp.from_list([(self.Pauli_ops[p].to_label(), reward_factor[i])
                                                for i, p in enumerate(pauli_index)])
-
         # Benchmarking block: not part of reward calculation
         # Apply parametrized quantum circuit (action), for benchmarking only
 
@@ -415,7 +416,6 @@ class QuantumEnvironment:
 
         # Build full quantum circuit: concatenate input state prep and parametrized unitary
         self.parametrized_circuit_func(qc)
-        qc = transpile(qc, self.backend, initial_layout=self.layout)
         job = self.estimator.run(circuits=[qc] * batch_size, observables=[observables] * batch_size,
                                  parameter_values=angles, shots=self.sampling_Pauli_space * self.n_shots,
                                  job_tags=[f"rl_qoc_step{self._step_tracker}"])
@@ -423,7 +423,7 @@ class QuantumEnvironment:
         reward_table = job.result().values
         self.reward_history.append(reward_table)
         assert len(reward_table) == batch_size
-        return reward_table  # Shape [batchsize]
+        return reward_table # Shape [batchsize]
 
     def _store_benchmarks(self, qc_list: List[QuantumCircuit]):
         """
@@ -484,7 +484,8 @@ class QuantumEnvironment:
         batch_size = len(qc_list)
 
         process_tomography_exps = BatchExperiment([ProcessTomography(qc, backend=self._benchmark_backend,
-                                                       physical_qubits=self.tgt_register) for qc in qc_list])
+                                                                     physical_qubits=self.tgt_register) for qc in
+                                                   qc_list])
 
         results = process_tomography_exps.run(self._benchmark_backend)
         process_results = [results.child_data(i).analysis_results(0) for i in range(batch_size)]
@@ -492,7 +493,7 @@ class QuantumEnvironment:
         avg_gate_fidelity = np.mean([average_gate_fidelity(Choi_matrix, Operator(self.target["gate"]))
                                      for Choi_matrix in Choi_matrices])
         prc_fidelity = np.mean([process_fidelity(Choi_matrix, Operator(self.target["gate"]))
-                                     for Choi_matrix in Choi_matrices])
+                                for Choi_matrix in Choi_matrices])
 
         self.process_fidelity_history.append(prc_fidelity)
         self.avg_fidelity_history.append(avg_gate_fidelity)
@@ -544,12 +545,28 @@ class QuantumEnvironment:
         self._n_qubits = n_qubits
 
     @property
+    def layout(self):
+        return self._layout
+
+    @layout.setter
+    def layout(self, layout: Layout):
+        self._layout = layout
+
+    @property
     def config_type(self):
         return self._config_type
 
     @property
     def config(self):
         return self._config
+
+    @property
+    def estimator(self):
+        return self._estimator
+
+    @estimator.setter
+    def estimator(self, estimator: BaseEstimator):
+        self._estimator = estimator
 
     @property
     def step_tracker(self):

torch_gate_calibration/torch_contextual_gate_cal.py
@@ -2,7 +2,7 @@ import os
 
 from qiskit.providers.models import QasmBackendConfiguration
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 # Qiskit imports
 from qiskit import pulse, transpile
 from qiskit.circuit import ParameterVector, QuantumCircuit, QuantumRegister, Gate, \
@@ -24,7 +24,6 @@ from gymnasium.spaces import Box, MultiDiscrete, Space
 import torch
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
-import wandb
 import torch.optim as optim
 from torch.distributions import MultivariateNormal, LowRankMultivariateNormal, Normal
 
@@ -32,6 +31,8 @@ import numpy as np
 import tqdm
 import time
 from typing import Union, Optional, List, Sequence
+
+
 # Save your credentials on disk.
 # IBMProvider.save_account(token='<IBM Quantum API key>')
 
@@ -68,20 +69,21 @@ def custom_pulse_schedule(backend: Union[BackendV1, BackendV2], qubit_tgt_regist
 
 
 def param_circuit(qc: QuantumCircuit,
-                  params: Optional[ParameterVector], q_reg: Optional[QuantumRegister]=None):
-
+                  params: Optional[ParameterVector], q_reg: Optional[QuantumRegister] = None):
     # To build a unique gate identifier, choose the name based on the input circuit name (by default and if used
     # with TFQuantumEnvironment class, this circuit name is of the form c_circ_trunc_{i})
     # custom_gate_name = f"{target['gate'].name}_{qc.name[-1]}"
-    # custom_gate = Gate(custom_gate_name, len(target["register"]), params=[param for param in params])
+    # custom_gate = Gate(custom_gate_name, len(target["register"]), params=params.params)
     # custom_sched = custom_pulse_schedule(backend=backend, qubit_tgt_register=physical_qubits, params=params,
     #                                      default_schedule=backend.target.get_calibration(target["gate"].name,
     #                                                                                      tuple(physical_qubits)))
     # qc.add_calibration(custom_gate, physical_qubits, custom_sched)
     # qc.append(custom_gate, physical_qubits)
-    qc.u(2 * np.pi * params[0], 2 * np.pi * params[1], 2 * np.pi * params[2], 0)
-    qc.u(2 * np.pi * params[3], 2 * np.pi * params[4], 2 * np.pi * params[5], 1)
-    qc.rzx(2 * np.pi * params[6], 0, 1)
+    # qc.u(np.pi * params[0], np.pi * params[1], np.pi * params[2], 0)
+    # qc.u(np.pi * params[3], np.pi * params[4], np.pi * params[5], 1)
+    # qc.rzx(np.pi * params[6], 0, 1)
+    qc.rx(np.pi*params[0], physical_qubits)
+
 
 """
 -----------------------------------------------------------------------------------------------------
@@ -95,43 +97,43 @@ N_shots = 1  # Number of shots for sampling the quantum computer for each action
 fake_backend = FakeJakartaV2()
 
 aer_backend = AerSimulator.from_backend(fake_backend, noise_model=None)
-backend = fake_backend
-target_gate = CXGate()
-physical_qubits = [0, 1]
+backend = aer_backend
+target_gate = XGate()
+physical_qubits = [0]
 n_qubits = len(physical_qubits)
 target = {"gate": target_gate, 'register': physical_qubits}
 config = QiskitConfig(parametrized_circuit=param_circuit, backend=backend)
-
 
 q_env = QuantumEnvironment(target, "circuit", config)
 
 # Circuit context
 seed = 10
 training_steps_per_gate = 1500
-target_circuit = transpile(random_circuit(4, depth=2, max_operands=2, seed=seed), backend)
-target_circuit = QuantumCircuit(2)
-target_circuit.cx(0, 1)
+# target_circuit = transpile(random_circuit(4, depth=2, max_operands=2, seed=seed), backend)
+target_circuit = QuantumCircuit(1)
+target_circuit.x(0)
 tgt_qubits = [target_circuit.qubits[i] for i in physical_qubits]
 
 tgt_instruction_counts = target_circuit.data.count(CircuitInstruction(target_gate, tgt_qubits))
 
-batchsize = 300  # Batch size (iterate over a bunch of actions per policy to estimate expected return) default 100
-n_actions = 7  # Choose how many control parameters in pulse/circuit parametrization
+batchsize = 400  # Batch size (iterate over a bunch of actions per policy to estimate expected return) default 100
+n_actions = 1  # Choose how many control parameters in pulse/circuit parametrization
 min_bound_actions = -1.
 max_bound_actions = 1.
-observation_space = Box(low=np.array([0, 0]), high=np.array([4**n_qubits, tgt_instruction_counts]), shape=(2,),
+observation_space = Box(low=np.array([0, 0]), high=np.array([4 ** n_qubits, tgt_instruction_counts]), shape=(2,),
                         seed=seed)
 action_space = Box(low=min_bound_actions, high=max_bound_actions, shape=(n_actions,), seed=seed)
 
 torch_env = TorchQuantumEnvironment(q_env, target_circuit, action_space, observation_space, batch_size=batchsize,
-                                    training_steps_per_gate=training_steps_per_gate, intermediate_rewards=False, seed=None)
+                                    training_steps_per_gate=training_steps_per_gate, intermediate_rewards=False,
+                                    seed=None)
 
 
 class ActorNetwork(nn.Module):
-    def __init__(self, observation_space: Space, hidden_layers:Sequence[int],
+    def __init__(self, observation_space: Space, hidden_layers: Sequence[int],
                  n_actions: int,
-                 hidden_activation_functions:Optional[Sequence[nn.Module]]=None,
-                 include_critic = True,
+                 hidden_activation_functions: Optional[Sequence[nn.Module]] = None,
+                 include_critic=True,
                  chkpt_dir: str = 'tmp/ppo'):
         super(ActorNetwork, self).__init__()
 
@@ -146,7 +148,7 @@ class ActorNetwork(nn.Module):
         layers = []
 
         # Iterate over the layer sizes to create the network layers
-        for i in range(len(layer_sizes)-1):
+        for i in range(len(layer_sizes) - 1):
             # Add a linear layer with the current and next layer sizes
             layers.append(nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
 
@@ -171,7 +173,6 @@ class ActorNetwork(nn.Module):
     def forward(self, x):
         x = self.base_network(x)
         mean_action = self.mean_action(x)
-
         std_action = self.std_action(x)
         std_action = self.std_activation(std_action)
         critic_output = self.critic_output(x)
@@ -185,10 +186,13 @@ class ActorNetwork(nn.Module):
         x = self.base_network(x)
         x = self.critic_output(x)
         return x
+
     def save_checkpoint(self):
         torch.save(self, self.checkpoint_dir)
+
     def load_checkpoint(self):
         torch.load(self.checkpoint_dir)
+
 
 class CriticNetwork(nn.Module):
     def __init__(self, observation_space: Space, hidden_layers: Sequence[int],
@@ -230,8 +234,10 @@ class CriticNetwork(nn.Module):
 
     def save_checkpoint(self):
         torch.save(self, self.checkpoint_dir)
+
     def load_checkpoint(self):
         torch.load(self.checkpoint_dir)
+
 
 class Agent(nn.Module):
     def __init__(self, actor_net: ActorNetwork, critic_net: Optional[CriticNetwork]):
@@ -242,6 +248,7 @@ class Agent(nn.Module):
 
         if self.critic_net is not None:
             assert not self.actor_net.include_critic, "Critic already included in Actor Network"
+
     def forward(self, x):
         if self.actor_net.include_critic:
             return self.actor_net(x)
@@ -258,11 +265,15 @@ class Agent(nn.Module):
             assert self.critic_net is not None, 'Critic Network not provided and not included in ActorNetwork'
             return self.critic_net(x)
 
+    def save_checkpoint(self):
+        self.actor_net.save_checkpoint()
+        if self.critic_net is not None:
+            self.critic_net.save_checkpoint()
 
 
-actor_net = ActorNetwork(observation_space, [50, 50], n_actions).to(device)
-critic_net = CriticNetwork(observation_space, [50, 50]).to(device)
-agent = Agent(actor_net, critic_net=None)
+actor_net = ActorNetwork(observation_space, [128, 128], n_actions, [nn.ELU(), nn.ELU(), nn.ELU()]).to(device)
+critic_net = CriticNetwork(observation_space, [128, 128], [nn.ELU(), nn.ELU(), nn.ELU()]).to(device)
+agent = Agent(actor_net, critic_net=None).to(device)
 """
 -----------------------------------------------------------------------------------------------------
 Hyperparameters for RL agent
@@ -275,8 +286,8 @@ writer = SummaryWriter(f"runs/{run_name}")
 #     "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
 # )
 # Hyperparameters for the agent
-n_epochs = 200  # Number of epochs : default 1500
-num_updates = 1000
+n_epochs = 10  # Number of epochs : default 1500
+num_updates = 5000
 opti = "Adam"
 lr_actor = 0.0005  # Learning rate for policy update step
 lr_critic = 0.0018  # Learning rate for critic (value function) update step
@@ -291,9 +302,9 @@ gamma = 1.
 gae_lambda = 0.95
 
 # Clipping
-clip_vloss = False
-grad_clip = 0.01
-clip_coef = 5.
+clip_vloss = True
+grad_clip = 0.05
+clip_coef = 0.5
 normalize_advantage = False
 
 # other coefficients
@@ -312,12 +323,14 @@ train_obs = torch.zeros((batchsize,) + torch_env.observation_space.shape, requir
 # Start the Environment
 start_time = time.time()
 
+
 for update in tqdm.tqdm(range(1, num_updates + 1)):
-    num_steps = torch_env.episode_length(global_step)
     next_obs, _ = torch_env.reset(seed=seed)
-    next_obs = torch.Tensor(next_obs).to(device)
+    num_steps = torch_env.episode_length(global_step)
+    next_obs = torch.Tensor([next_obs]*batchsize).to(device)
     next_done = torch.zeros(batchsize).to(device)
-    print("episode length:", num_steps)
+
+    # print("episode length:", num_steps)
 
     for step in range(num_steps):
         global_step += 1
@@ -327,19 +340,21 @@ for update in tqdm.tqdm(range(1, num_updates + 1)):
         with torch.no_grad():
             mean_action, std_action, critic_value = agent(next_obs)
             probs = Normal(mean_action, std_action)
-            action = probs.sample(torch.Size((batchsize,)))
+            action = torch.clip(probs.sample(), min_bound_actions, max_bound_actions)
             logprob = probs.log_prob(action).sum(1)
+            values[step] = critic_value.flatten()
 
         actions[step] = action
         logprobs[step] = logprob
+        # next_obs, reward, terminated, truncated, infos = torch_env.step(action.cpu().numpy())
         next_obs, reward, terminated, truncated, infos = torch_env.step(action.cpu().numpy())
         done = np.logical_or(terminated, truncated)
         rewards[step] = torch.tensor(reward).to(device)
-        next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(np.array([int(done)]*batchsize)).to(device)
-
+        next_obs = torch.Tensor(np.array([next_obs]*batchsize)).to(device)
+        next_done = torch.Tensor(np.array([int(done)] * batchsize)).to(device)
         # Only print when at least 1 env is done
 
-        print(f"global_step={global_step}, episodic_return={np.mean(reward)}")
+        #print(f"global_step={global_step}, episodic_return={np.mean(reward)}")
         writer.add_scalar("charts/episodic_return", np.mean(reward), global_step)
         writer.add_scalar("charts/episodic_length", num_steps, global_step)
 
@@ -377,10 +392,16 @@ for update in tqdm.tqdm(range(1, num_updates + 1)):
             mb_inds = b_inds[start:end]
             new_mean, new_sigma, new_value = agent(b_obs[mb_inds])
             new_dist = Normal(new_mean, new_sigma)
+            print('new_mean', new_mean)
+            print('new_sigma', new_sigma)
+            print('new_value', new_value)
             new_logprob, entropy = new_dist.log_prob(b_actions[mb_inds]).sum(1), new_dist.entropy().sum(1)
             logratio = new_logprob - b_logprobs[mb_inds]
+            print("new_logprob", new_logprob)
+            print('b_logprob', b_logprobs[mb_inds])
+            print("logratio", logratio)
             ratio = logratio.exp()
-
+            print("ratio", ratio)
             with torch.no_grad():
                 # calculate approx_kl http://joschu.net/blog/kl-approx.html
                 old_approx_kl = (-logratio).mean()
@@ -388,7 +409,7 @@ for update in tqdm.tqdm(range(1, num_updates + 1)):
                 clipfracs += [((ratio - 1.0).abs() > epsilon).float().mean().item()]
 
             mb_advantages = b_advantages[mb_inds]
-            if normalize_advantage: # Normalize advantage
+            if normalize_advantage:  # Normalize advantage
                 mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
 
             # Policy loss
@@ -422,21 +443,21 @@ for update in tqdm.tqdm(range(1, num_updates + 1)):
     y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
     var_y = np.var(y_true)
     explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
-    print("Average return:", np.mean(torch_env.reward_history, axis =1)[-1])
+    print("Average return:", np.mean(torch_env.reward_history, axis=1)[-1])
     # print(np.mean(torch_env.reward_history, axis =1)[-1])
-    print("Average gate fidelity:", torch_env.avg_fidelity_history[-1])
+    print("Circuit fidelity:", torch_env.circuit_fidelity_history[-1])
     # TRY NOT TO MODIFY: record rewards for plotting purposes
     writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
     writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
-    writer.add_scalar("losses/avg_return", np.mean(torch_env.reward_history, axis =1)[-1], global_step)
-    writer.add_scalar("losses/avg_gate_fidelity", torch_env.avg_fidelity_history[-1], global_step)
+    writer.add_scalar("losses/avg_return", np.mean(torch_env.reward_history, axis=1)[-1], global_step)
+    # writer.add_scalar("losses/avg_gate_fidelity", torch_env.avg_fidelity_history[-1], global_step)
+    writer.add_scalar("losses/circuit_fidelity", torch_env.circuit_fidelity_history[-1], global_step)
     writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
     writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
     writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
     writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
     writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
     writer.add_scalar("losses/explained_variance", explained_var, global_step)
-
 
 torch_env.close()
 writer.close()