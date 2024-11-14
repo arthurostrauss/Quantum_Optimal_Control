8d0dc04e7396652204d4407e3cfbe5ab9c9de81f
Arthur Strauss <arthurostrauss@gmail.com>
2024-11-14 17:12:55 +0800

    Moved QUA dedicated pass in subfolder

--- rl_qoc/helpers/transpiler_passes.py
+++ rl_qoc/helpers/transpiler_passes.py
@@ -47,43 +47,6 @@ class MomentAnalysisPass(AnalysisPass):
         )
 
 
-class QuaParameterTablePass(AnalysisPass):
-    """
-    This pass is used to generate a ParameterTable object from the parameters of a list of circuits and
-    adds it to the property set of the PassManager.
-    """
-
-    def __init__(self):
-        super().__init__()
-        self.parameter_tables = []
-
-    def run(self, circuit):
-        # Get the parameters from the circuit.
-        parameters = circuit.parameters
-        param_dict = {}
-        if parameters:
-
-            # If the circuit has parameters, convert them to a dictionary.
-            for parameter in parameters:
-                if (
-                    isinstance(parameter, ParameterVectorElement)
-                    and parameter.vector.name not in param_dict
-                ):
-                    param_dict[parameter.vector.name] = [
-                        0.0 for _ in range(len(parameter.vector))
-                    ]
-                elif isinstance(parameter, Parameter):
-                    param_dict[parameter.name] = 0.0
-
-            # Create a ParameterTable object.
-            param_table = ParameterTable(param_dict)
-            self.parameter_tables.append(param_table)
-            # Add the ParameterTable object to the property set
-            self.property_set["parameter_table"] = self.parameter_tables
-
-        return circuit
-
-
 def format_input(input, input_type):
     """
     Format the input to the required type
---
+++ rl_qoc/qua/transpiler_passes.py
@@ -0,0 +1,41 @@
+from qiskit.circuit.parameter import Parameter
+from qiskit.circuit.parametervector import ParameterVectorElement
+from qiskit.transpiler.basepasses import AnalysisPass
+from qualang_tools.video_mode import ParameterTable
+
+
+class QuaParameterTablePass(AnalysisPass):
+    """
+    This pass is used to generate a ParameterTable object from the parameters of a list of circuits and
+    adds it to the property set of the PassManager.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.parameter_tables = []
+
+    def run(self, circuit):
+        # Get the parameters from the circuit.
+        parameters = circuit.parameters
+        param_dict = {}
+        if parameters:
+
+            # If the circuit has parameters, convert them to a dictionary.
+            for parameter in parameters:
+                if (
+                    isinstance(parameter, ParameterVectorElement)
+                    and parameter.vector.name not in param_dict
+                ):
+                    param_dict[parameter.vector.name] = [
+                        0.0 for _ in range(len(parameter.vector))
+                    ]
+                elif isinstance(parameter, Parameter):
+                    param_dict[parameter.name] = 0.0
+
+            # Create a ParameterTable object.
+            param_table = ParameterTable(param_dict)
+            self.parameter_tables.append(param_table)
+            # Add the ParameterTable object to the property set
+            self.property_set["parameter_table"] = self.parameter_tables
+
+        return circuit
